6d62ed41cb19cd7c6c9aa905cd406559f73dbf86
vfxsnake <jazzjoe@gmail.com>
2020-06-01 20:56:06 -0500

    Chapter 3 p.117 simple movemetns and sprites implementation.

--- Source.cpp
+++ Source.cpp
@@ -16,6 +16,10 @@ sf::Sprite BgSprite;
 //Hero Layer
 sf::Texture HeroTexture;
 sf::Sprite HeroSprite;
+sf::Vector2f PlayerPosition;
+//bool player moving
+bool PlayerMoving = false;
+
 
 // Function to initialze textures and Sprites
 void Init()
@@ -31,6 +35,7 @@ void Init()
 	HeroSprite.setPosition(sf::Vector2f(ViewSize.x/2, ViewSize.y/2));
 	// the size parameter comes from the texture size
 	HeroSprite.setOrigin(HeroTexture.getSize().x/2, HeroTexture.getSize().y/2);
+
 }
 
 void Draw()
@@ -40,17 +45,62 @@ void Draw()
 	Window.draw(HeroSprite);
 }
 
+void UpdateInput()
+{
+	sf::Event Event;
+	while (Window.pollEvent(Event))
+	{
+		// key press event:
+		if (Event.type == sf::Event::KeyPressed)
+		{
+			if (Event.key.code == sf::Keyboard::Right)
+			{
+				PlayerMoving = true;
+			}
+		}
+		// key released event
+		if (Event.type == sf::Event::KeyReleased)
+		{
+			if (Event.key.code == sf::Keyboard::Right)
+			{
+				PlayerMoving = false;
+			}
+		}
+
+		// handle the exit of the game closing the window.
+		if (Event.key.code == sf::Keyboard::Escape || Event.type == sf::Event::Closed)
+		{
+			Window.close();
+		}
+	}
+}
+
+void Update(float Dt)
+{
+	if (PlayerMoving)
+	{
+		HeroSprite.move(50.0f * Dt, 0);
+	}
+}
+
 int main()
 {
+	//initilize clock
+	sf::Clock Clock;
+
 	// Initialize Game Objects:
 	Init();
 
-	
 	// Game loop will begin here
 	while (Window.isOpen())
 	{
 		//Handle Keyboard Events
-		
+		UpdateInput();
+
+		// update game before drawing
+		sf::Time Dt = Clock.restart();
+		Update(Dt.asSeconds());
+
 		//Upadte Game Objects in the sene
 		Window.clear(sf::Color::Red);
 
